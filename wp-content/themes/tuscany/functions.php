9ebcc7284ccc2c3c200b3d99e2ea19b2785a62f2
William Ortega <william.ortega@outlook.com>
2016-05-26 23:41:14 -0600

    Updated show categories and sub-categories in product page.

--- wp-content/themes/tuscany/functions.php
+++ wp-content/themes/tuscany/functions.php
@@ -309,3 +309,137 @@ remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_ca
 
 //display 16 products per page
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 16;' ), 20 );
+
+
+// Redefine sub category display to output empty categories
+function woocommerce_product_subcategories( $args = array() ) {
+	global $wp_query;
+
+	$defaults = array(
+		'before'        => '',
+		'after'         => '',
+		'force_display' => false
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	extract( $args );
+
+	// Main query only
+	if ( ! is_main_query() && ! $force_display ) {
+		return;
+	}
+
+	// Don't show when filtering, searching or when on page > 1 and ensure we're on a product archive
+	if ( is_search() || is_filtered() || is_paged() || ( ! is_product_category() && ! is_shop() ) ) {
+		return;
+	}
+
+	// Check categories are enabled
+	if ( is_shop() && '' === get_option( 'woocommerce_shop_page_display' ) ) {
+		return;
+	}
+
+	// Find the category + category parent, if applicable
+	$term 			= get_queried_object();
+	$parent_id 		= empty( $term->term_id ) ? 0 : $term->term_id;
+
+	if ( is_product_category() ) {
+		$display_type = get_woocommerce_term_meta( $term->term_id, 'display_type', true );
+
+		switch ( $display_type ) {
+			case 'products' :
+				return;
+				break;
+			case '' :
+				if ( '' === get_option( 'woocommerce_category_archive_display' ) ) {
+					return;
+				}
+				break;
+		}
+	}
+
+	// NOTE: using child_of instead of parent - this is not ideal but due to a WP bug ( http://core.trac.wordpress.org/ticket/15626 ) pad_counts won't work
+	$product_categories_final = get_categories( apply_filters( 'woocommerce_product_subcategories_args', array(
+		'parent'       => $parent_id,
+		'menu_order'   => 'ASC',
+		'hide_empty'   => 0,
+		'hierarchical' => 1,
+		'taxonomy'     => 'product_cat',
+		'pad_counts'   => 1
+	) ) );
+
+	// get all term_id
+	$terms_id = array_map(function($term) {
+		return $term->term_id;
+	}, $product_categories_final);
+
+	// run each term for find subcategories
+	$product_subcategories = array();
+	for ($i = 0; $i < count($terms_id); $i++) {
+		$product_subcategories[$i] = get_categories( apply_filters( 'woocommerce_product_subcategories_args', array(
+			'child_of'       => (int) $terms_id[$i],
+			'menu_order'   => 'ASC',
+			'hide_empty'   => 0,
+			'hierarchical' => 1,
+			'taxonomy'     => 'product_cat',
+			'pad_counts'   => 1
+		) ) );
+		if (empty($product_subcategories[$i])) {
+			unset($product_subcategories[$i]);
+		}
+	}
+
+	// save master array
+	$product_categories = array();
+	foreach ($terms_id as $id => $term) {
+		if (isset($product_subcategories[$id])) {
+			foreach ($product_subcategories[$id] as $cat) {
+				array_push($product_categories, $cat);
+			}
+		} else {
+			array_push($product_categories, $product_categories_final[$id]);
+		}
+	}
+
+	if ( ! apply_filters( 'woocommerce_product_subcategories_hide_empty', false ) ) {
+		$product_categories = wp_list_filter( $product_categories, array( 'count' => 0 ), 'NOT' );
+	}
+
+	if ( $product_categories ) {
+		echo $before;
+
+		foreach ( $product_categories as $category ) {
+			wc_get_template( 'content-product_cat.php', array(
+				'category' => $category
+			) );
+		}
+
+		// If we are hiding products disable the loop and pagination
+		if ( is_product_category() ) {
+			$display_type = get_woocommerce_term_meta( $term->term_id, 'display_type', true );
+
+			switch ( $display_type ) {
+				case 'subcategories' :
+					$wp_query->post_count    = 0;
+					$wp_query->max_num_pages = 0;
+					break;
+				case '' :
+					if ( 'subcategories' === get_option( 'woocommerce_category_archive_display' ) ) {
+						$wp_query->post_count    = 0;
+						$wp_query->max_num_pages = 0;
+					}
+					break;
+			}
+		}
+
+		if ( is_shop() && 'subcategories' === get_option( 'woocommerce_shop_page_display' ) ) {
+			$wp_query->post_count    = 0;
+			$wp_query->max_num_pages = 0;
+		}
+
+		echo $after;
+
+		return true;
+	}
+}
